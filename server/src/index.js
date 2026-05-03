ef27237b30bd629008029e65e354640e47193da4
Arooba-Iqbal <aroobaiqbal1058@gmail.com>
2026-05-03 23:01:10 +0500

    Making Project Deployment Ready

--- server/src/index.js
+++ server/src/index.js
@@ -13,7 +13,11 @@ connectDb();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+//app.use(cors());
+app.use(cors({
+  origin: "*", // later restrict to Vercel domain
+  credentials: true
+}));
 app.use(express.json());
 
 app.get("/api/health", (_req, res) => {
