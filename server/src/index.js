d2328ee8e091c51090ceb2805287fa33336c44a7
Arooba-Iqbal <aroobaiqbal1058@gmail.com>
2026-05-03 23:47:23 +0500

    Index file changed

--- server/src/index.js
+++ server/src/index.js
@@ -15,8 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 //app.use(cors());
 app.use(cors({
-  origin: "*", // later restrict to Vercel domain
-  credentials: true
+  origin: "*"
 }));
 app.use(express.json());
 
