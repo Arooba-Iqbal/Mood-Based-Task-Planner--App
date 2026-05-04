5e220e360240e894b5c7201bd57a86628664d2a5
Arooba-Iqbal <aroobaiqbal1058@gmail.com>
2026-05-04 21:42:05 +0500

    Some more changes

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
+const apiBase = import.meta.env.VITE_API_URL || "http://localhost:5000";
+
 const api = axios.create({
-  //baseURL: "http://localhost:5000/api"
-  baseURL: "https://mood-based-task-planner-app-vn8o.vercel.app/api"
+  baseURL: `${apiBase}/api`
 });
 
 api.interceptors.request.use((config) => {
