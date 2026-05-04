ec1c358341a46bdbab207720f4a90c2027329a08
Rahila-Iqbal <rahilaiqbal161@gmail.com>
2026-05-04 22:18:34 +0500

    render_issue

--- server/src/index.js
+++ server/src/index.js
@@ -36,10 +36,12 @@ app.use((error, _req, res, _next) => {
 // app.listen(PORT, () => {
 //   console.log(`Server is running on port ${PORT}`);
 // });
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-}
-
+//if (process.env.NODE_ENV !== 'production') {
+//  app.listen(PORT, () => {
+ //   console.log(`Server is running on port ${PORT}`);
+ // });
+//}
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 export default app;
