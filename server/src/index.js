db936a37ccf1563289e477cf515d53c532de19ac
Rahila-Iqbal <rahilaiqbal161@gmail.com>
2026-05-04 22:38:00 +0500

    new_index_change

--- server/src/index.js
+++ server/src/index.js
@@ -49,13 +49,6 @@ export default app;
 
 
 
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-}
-
-
 
 export default app;
 
