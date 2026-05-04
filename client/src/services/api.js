27da91102378656b2444694ee31e2a07fc5c6a64
Rahila-Iqbal <rahilaiqbal161@gmail.com>
2026-05-04 23:19:57 +0500

    switch API to env-based backend URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  //baseURL: "http://localhost:5000/api"
-  baseURL: "https://mood-based-task-planner-app-vn8o.vercel.app/api"
+  baseURL: `${import.meta.env.VITE_API_URL}/api`
 });
 
 api.interceptors.request.use((config) => {
